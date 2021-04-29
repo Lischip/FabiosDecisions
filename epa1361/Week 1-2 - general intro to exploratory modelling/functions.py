b092d2a070c7e8279e49524196de3d1ada286714
Lischip <lis.94@live.nl>
2021-04-29 14:55:01 +0200

    2 barrel rolls?

--- epa1361/Week 1-2 - general intro to exploratory modelling/functions.py
+++ epa1361/Week 1-2 - general intro to exploratory modelling/functions.py
@@ -9,3 +9,9 @@ def process_data(experiments, outcomes):
     data = pd.DataFrame(outcomes)
     data['policy'] = policies
     return data
+
+def process_data2(experiments, outcomes):
+    df = pd.DataFrame.from_dict(outcomes)
+    df = df.assign(policy=experiments['policy'])
+    df['policy'] = df['policy'].map({p: i for i, p in enumerate(set(experiments['policy']))})
+    return df
